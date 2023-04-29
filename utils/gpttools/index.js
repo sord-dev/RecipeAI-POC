82eeb37a49a5aea79edd21699819e4f582a11f7e
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-29 23:27:21 +0100

    add more comments to gpttools module

--- utils/gpttools/index.js
+++ utils/gpttools/index.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const fs = require('fs');
-const prefix = fs.readFileSync(__dirname + '/prefix.txt').toString(); // gpt prefix file to establish foods, recipes and the dataformat command 
+const prefix = fs.readFileSync(__dirname + '/prefix.txt').toString(); // gpt prefix file to establish foods, recipes and our dataformat command 
 const { Configuration, OpenAIApi } = require('openai');
 
 // openai setup for and helper functions for ChatGPT
 const config = new Configuration({ apiKey: process.env.OPEN_AI_API_KEY })
 const OpenAPI = new OpenAIApi(config);
 
-OpenAPI.command = async (content) => { // helper command to easily command gpt
+OpenAPI.command = async (content) => { // custom helper command to easily command gpt with our dataformat and json structure
     let cmd = { role: "user", content: prefix + ' ' + content };
     const res = await OpenAPI.createChatCompletion({ model: 'gpt-3.5-turbo', messages: [cmd] });
 
     return res.data;
 }
 
-module.exports.OpenAPI = OpenAPI;
-module.exports.parseGPT = require('./parseGPT');
+module.exports.OpenAPI = OpenAPI; // what we use to interface with chatgpt
+module.exports.parseGPT = require('./parseGPT'); // what we use to parse our dataformat from chatgpt
