e7b69623b8f93492e4d917c00c6ae5d2fb8a8bd9
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-29 23:15:25 +0100

    change init.txt to prefix.txt due to usage change

--- utils/gpttools/index.js
+++ utils/gpttools/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
-const payload = fs.readFileSync(__dirname + '/init.txt').toString(); // gpt init file to establish foods, recipes and the dataformat command 
+const prefix = fs.readFileSync(__dirname + '/prefix.txt').toString(); // gpt prefix file to establish foods, recipes and the dataformat command 
 const { Configuration, OpenAIApi } = require('openai');
 
 // openai setup for and helper functions for ChatGPT
@@ -8,11 +8,10 @@ const config = new Configuration({ apiKey: process.env.OPEN_AI_API_KEY })
 const OpenAPI = new OpenAIApi(config);
 
 OpenAPI.command = async (content) => { // helper command to easily command gpt
-    let cmd = { role: "user", content: payload + ' ' + content };
+    let cmd = { role: "user", content: prefix + ' ' + content };
     const res = await OpenAPI.createChatCompletion({ model: 'gpt-3.5-turbo', messages: [cmd] });
 
     return res.data;
-
 }
 
 module.exports.OpenAPI = OpenAPI;
